96debba382736248f8ac256218751dcc84a04524
Gamze Aslan <95714545+gamzeaslan@users.noreply.github.com>
2023-01-15 20:56:06 +0300

    Update github_followers_list.py

--- github_followers_list.py
+++ github_followers_list.py
@@ -29,7 +29,7 @@ class GitHub:
         button.click()
         time.sleep(3)
     
-    def repoSearch(self,keyword):#just one page ??
+    def repoSearch(self,keyword,page):#adds the repo on the page to the list as many as the number of pages entered
         #find search bar
         searchInput=self.browser.find_element(By.NAME,"q")
         
@@ -39,19 +39,26 @@ class GitHub:
         #press enter
         searchInput.send_keys(Keys.ENTER)
         time.sleep(3)
+        self.repo_link=[]
+        i=1       
+        while i<page:
+            self.browser.get("https://github.com/search?p="+str(i)+"&q=python&type=Repositories")
+            i+=1
+            time.sleep(2)#load wait 
+            repos=self.browser.find_elements(By.XPATH,"/html/body/div[1]/div[5]/main/div/div[3]/div/ul/li")
+            for repo in repos :
+                self.repo_link.append(repo.find_element(By.TAG_NAME,'a').get_attribute("href"))
+
+    
         
-        repos=self.browser.find_elements(By.XPATH,"/html/body/div[1]/div[5]/main/div/div[3]/div/ul/li")
-        for repo in repos :
-            print(repo.find_element(By.TAG_NAME,'a').text)
-            print(repo.find_element(By.TAG_NAME,'a').get_attribute("href"))
-            print("---------------")
         
         
         
 #create object
 github=GitHub(username,password)
 github.signIn()
-github.repoSearch("python")
+github.repoSearch("python",5)
+print(github.repo_link)
         
         
         
